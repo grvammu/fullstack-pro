13e0bf02993856102478ffe29afadb73a1a49bd4
grvammu <112574845+grvammu@users.noreply.github.com>
2024-08-22 01:45:07 +0530

    Update offers.tf

--- terraform/offers.tf
+++ terraform/offers.tf
@@ -23,7 +23,7 @@ resource "kubernetes_deployment" "offer" {
       }
       spec {
         container {
-          image = "shobana56it/spring:v1"
+          image = "ammudivy/spring:v1"
           name  = "offer"
           port {
             container_port = 1001
